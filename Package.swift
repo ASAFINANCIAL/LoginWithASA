d4502b8c2e162a21c1069241ebede02b17dadd33
MykolaAsaFinancial <113994195+MykolaAsaFinancial@users.noreply.github.com>
2024-05-02 16:55:03 +0300

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LoginWithASA",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "LoginWithASA",
+            targets: ["LoginWithASA"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "LoginWithASA"),
+        .testTarget(
+            name: "LoginWithASATests",
+            dependencies: ["LoginWithASA"]),
+    ]
+)
